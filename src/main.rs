b3e4c2292d70f8c1bb5619dccefa3cd546cb6f07
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-09-27 14:51:27 -0400

    Generate some random vectors.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate rand;
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+  let args : Vec<String> = env::args().collect();
+  let seed : u64 = args[1].parse::<u64>().unwrap();
+  let mut rng : StdRng = SeedableRng::seed_from_u64(seed);
+  let mut vec : Vec<i8> = vec![1, 2, 3];
+  for x in 0..10 {
+    // Generate random demand vectors
+    // with random bids.
+    for i in 0..vec.len() {
+      vec[i] = rng.gen(); 
+    }
+    println!("Hello, world {}, random vector: {:?}!", x, vec);
+  }
 }
