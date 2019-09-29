dc756d524bf90ef2d3472fc2c79edb887ea90f9e
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-09-28 23:25:07 -0400

    Specify number of resources on cmdline.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::env;
 fn main() {
   let args : Vec<String> = env::args().collect();
   let seed : u64 = args[1].parse::<u64>().unwrap();
+  let num_resources : u64 = args[2].parse::<u64>().unwrap();
   let mut rng : StdRng = SeedableRng::seed_from_u64(seed);
-  let mut vec : Vec<i8> = vec![1, 2, 3];
-  for x in 0..10 {
+  for x in 0..100 {
     // Generate random demand vectors
-    // with random bids.
-    for i in 0..vec.len() {
-      vec[i] = rng.gen(); 
+    let mut vec : Vec<i16> = Vec::new();
+    for _y in 0..num_resources {
+      vec.push(rng.gen_range(0, 1000));
     }
     println!("Hello, world {}, random vector: {:?}!", x, vec);
   }
