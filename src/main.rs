855f9e163b4beb19815498d072412a704171bd45
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-10-15 16:37:10 -0400

    Fix integers widths and remove Option type.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use rand::rngs::StdRng;
 use std::env;
 
 struct Package {
-  cpu : u8,
-  mem : u8,
-  net : u8,
+  cpu : i32,
+  mem : i32,
+  net : i32,
 }
 
-type Valuation = Vec<(Package, Option<u32>)>;
+type Valuation = Vec<(Package, i32)>;
 
 // Use definition of AgentType from Parkes and Singh, 2003
 // "An MDP-Based Approach to Online Mechanism Design"
 struct AgentType {
-  arrival   : u16,
-  departure : u16,
+  arrival   : i32,
+  departure : i32,
   valuation : Valuation
 }
 
@@ -26,8 +26,8 @@ fn generate_random_agent_type(rng : &mut StdRng) -> AgentType {
 
 fn main() {
   let args : Vec<String> = env::args().collect();
-  let seed : u64 = args[1].parse::<u64>().unwrap();
-  let num_resources : u64 = args[2].parse::<u64>().unwrap();
+  let seed : i64 = args[1].parse::<i64>().unwrap();
+  let num_resources : i64 = args[2].parse::<i64>().unwrap();
   let mut rng : StdRng = SeedableRng::seed_from_u64(seed);
   for x in 0..100 {
     // Generate random demand vectors
