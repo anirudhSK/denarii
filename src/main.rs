d89d0f7cc083740691b8a8aaf76807b2a13324a6
Taegyun Kim <k.taegyun@gmail.com>
2019-11-27 14:25:08 -0500

    Use Clap to parse args.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
-extern crate rand;
 mod algorithms;
 mod gurobi;
-use rand::{Rng, SeedableRng};
+
+extern crate clap;
+
+use clap::{App, Arg};
 use rand::rngs::StdRng;
-use std::env;
+use rand::{Rng, SeedableRng};
+
+// Args for the program
+// n the number of packets to generate
+// lambda the average packet inter arrival time (in msec?) for poisson
+// distribution
+// mu the average packet processing time (in msec)
+
 fn main() {
-  let args : Vec<String> = env::args().collect();
-  let seed : u64 = args[1].parse::<u64>().unwrap();
-  let num_resources : u64 = args[2].parse::<u64>().unwrap();
-  let mut rng : StdRng = SeedableRng::seed_from_u64(seed);
-  for x in 0..100 {
-    // Generate random demand vectors
-    let mut vec : Vec<i16> = Vec::new();
-    for _y in 0..num_resources {
-      vec.push(rng.gen_range(0, 1000));
+    let matches = App::new("Denarii")
+        .version("0.1.0")
+        .author("Taegyun Kim <k.taegyun@gmail.com>")
+        .about("Packet based network device resource allocation simualtor.")
+        .arg(
+            Arg::with_name("seed")
+                .short("s")
+                .long("seed")
+                .takes_value(true)
+                .help("Random seed"),
+        )
+        .get_matches();
+
+    let seed: u64 = matches
+        .value_of("seed")
+        .unwrap_or("1")
+        .parse::<u64>()
+        .unwrap();
+    let mut rng: StdRng = SeedableRng::seed_from_u64(seed);
+
+    let num_resources: u64 = 2;
+    for x in 0..100 {
+        // Generate random demand vectors
+        let mut vec: Vec<i16> = Vec::new();
+        for _y in 0..num_resources {
+            vec.push(rng.gen_range(0, 1000));
+        }
+        println!("Hello, world {}, random vector: {:?}!", x, vec);
     }
-    println!("Hello, world {}, random vector: {:?}!", x, vec);
-  }
 }
