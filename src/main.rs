2d6a65bb8bd25146f2cb424986788407c2131968
Taegyun Kim <k.taegyun@gmail.com>
2019-12-08 01:13:29 -0500

    Simulation outline.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use clap::{App, Arg};
 use rand::distributions::{Bernoulli, Distribution};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
+use simulator::Packet;
+use std::collections::HashSet;
 
 // Args for the program
 // n the number of packets to generate
@@ -41,22 +43,51 @@ fn main() {
 
     let ticks = matches.value_of("ticks").unwrap().parse::<u64>().unwrap();
 
+    let mut allocated_pkts: Vec<&mut Packet> = Vec::new();
+    let mut pkts: Vec<Packet> = Vec::new();
+
     let p = 0.3;
     // Distribution for packet arrivals.
     let a_dist = Bernoulli::new(p).unwrap();
     let num_resources = 2;
-    let mut cnt = 0;
+
+    let capacity: Vec<f64> = (0..num_resources).map(|x| (x as f64) * 10.0).collect();
+    let mut available: Vec<f64> = capacity;
+    let mut latencies: Vec<u64> = Vec::new();
+
+    let alg = algorithms::Drf {};
+
+    let mut num_pkts = 0;
     for t in 0..ticks {
-        let value = a_dist.sample(&mut rng);
-        if value {
-            let service_time = rng.gen_range(10, 20);
+        let add_new_packet: bool = a_dist.sample(&mut rng);
+        // New Packet coming
+        if add_new_packet {
+            let service_time = rng.gen_range(10, 20) as f64;
             let resource_req: Vec<f64> = (0..num_resources)
                 .map(|_| rng.gen_range(1, 11) as f64)
                 .collect();
             println!("{}, {}, {:?}", t, service_time, resource_req);
-            cnt += 1;
+
+            let mut p: Packet = Packet::new(num_pkts, t, service_time, resource_req);
+            num_pkts += 1;
+            pkts.push(p);
         }
+
+        // Step each packet.
+        let mut done_pkts = 0;
+        for packet in &mut allocated_pkts {
+            let done = packet.step(t);
+
+            if done {
+                latencies.push(packet.latency());
+                done_pkts += 1
+            }
+        }
+        // Check whether a new allocation needs to happen
+        if add_new_packet || done_pkts > 0 {}
+
+        // Run the algorithm
     }
 
-    println!("{}: Total number of packets", cnt);
+    println!("{}: Total number of packets", num_pkts);
 }
--- src/simulator/mod.rs
+++ src/simulator/mod.rs
@@ -1,3 +1,5 @@
+pub use self::packet::*;
 pub use self::sim::*;
+
 pub mod packet;
 pub mod sim;
